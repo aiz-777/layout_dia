fdd8eae76989701d84bd332a4f47e10b9bc9525d
Andrii Zhurbas <zhurbas777@gmail.com>
2026-06-21 20:47:52 +0300

    implement blocks: header/who-we-are/expertise/process

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const sliderPhoto = document.querySelector('.header__photo');
+const prevButton = document.querySelector('.header__slider-button--prev');
+const nextButton = document.querySelector('.header__slider-button--next');
+
+const slideClasses = [
+  'header__photo--slide-1',
+  'header__photo--slide-2',
+  'header__photo--slide-3',
+];
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  sliderPhoto.classList.remove(...slideClasses);
+
+  currentSlide = (index + slideClasses.length) % slideClasses.length;
+
+  sliderPhoto.classList.add(slideClasses[currentSlide]);
+}
+
+prevButton.addEventListener('click', () => {
+  showSlide(currentSlide - 1);
+});
+
+nextButton.addEventListener('click', () => {
+  showSlide(currentSlide + 1);
+});
+
+
